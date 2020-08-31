e45683e46183e28daf527033d7973cbfb353f555
Leisten19 <david.leisten@elev.ga.ntig.se>
2020-08-31 12:45:42 +0200

    Variabler och datatyper
    variabler och datattyper

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,22 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Niklas!");
+            Console.WriteLine("Hello skriv in ditt namn");
+            var username = Console.ReadLine();
+            Console.WriteLine("Ditt namn är: " + username);
+            Console.WriteLine("skriv in din ålder");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Din ålder är: " + age);
+
+            Console.WriteLine("Är du vid liv?");
+            var alive = Console.ReadLine();
+            var ja = true;
+            var nej = false;
+            Console.WriteLine(ja);
+            Console.WriteLine(nej);
+
+
         }
     }
 }
+
