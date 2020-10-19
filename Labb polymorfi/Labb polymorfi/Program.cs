d1a1c07fef9bad7b8d32d9f2978f93e6524bae36
Leisten19 <david.leisten@elev.ga.ntig.se>
2020-10-19 14:01:53 +0200

    polymorfi

---
+++ Labb polymorfi/Labb polymorfi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Labb_polymorfi
+{
+    class Produkt
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
